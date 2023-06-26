72b7f903e6f9bd411c9342464a57928c6ac7c261
sethcarney <swc20a@fsu.edu>
2023-06-26 10:32:24 -0400

    Add Library to WEB API

---
+++ PracticeManagement.API/Controllers/ClientController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticeManagement.API.Database;
+using PracticeManagement.Library.Models;
+
+
+namespace PracticeManagement.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+  
+
+        public class ClientController : ControllerBase
+        {
+
+
+            private readonly ILogger<ClientController> _logger;
+
+            public ClientController(ILogger<ClientController> logger)
+            {
+                _logger = logger;
+            }
+
+            [HttpGet]
+            public IEnumerable<Client> Get()
+            {
+                return FakeDatabase.Clients;
+            }
+
+        }
+
+   
+
+}
---
+++ PracticeManagement.API/Database/FakeDatabase.cs
@@ -0,0 +1,14 @@
+﻿using PracticeManagement.Library.Models;
+
+namespace PracticeManagement.API.Database
+{
+    public class FakeDatabase
+    {
+        public static List<Client> Clients = new List<Client>
+        {
+             new Client { Id = 1, Name = "Client 1"},
+              new Client { Id = 2, Name = "Client 2"}
+
+        };
+    }
+}
